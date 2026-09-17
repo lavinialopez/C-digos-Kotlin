331c25a89ac978a4e9b0201581162e0fa9568a49
lavinialopez <akemidevilman@gmail.com>
2026-09-17 10:27:59 -0300

    Delete dadinho.py

--- dadinho.py
+++
@@ -1,7 +0,0 @@
-import kotlin.random.Random # type: ignore
-
-fun main() { # type: ignore
-    var numero = Random.nextInt(1, 6) # type: ignore
-
-    println("Número sorteado: $numero") # type: ignore
-} # type: ignore
