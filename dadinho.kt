f56d5c48fa46b15fd153a0cf1660b00948bc3cd1
lavinialopez <akemidevilman@gmail.com>
2026-09-17 10:58:28 -0300

    Alterações dos comentários

--- dadinho.kt
+++ dadinho.kt
@@ -1,7 +1,7 @@
 import kotlin.random.Random
 
 fun main() { //definimos a funçao principal iiiiiiiiiirra
-    var numero = Random.nextInt(1, 6) //aqui criamos uma variavel que pode mudar seu valor!!!!!!1 sorteando entre 1 a 6
+    var numero = Random.nextInt(1, 7) //aqui criamos uma variavel que pode mudar seu valor!!!!!!1 sorteando entre 1 a 6
 
     println("Número sorteado: $numero") //aqui usamos o print line para mostrar o número escolhido!
 }
--- nomeslegais.kt
+++ nomeslegais.kt
@@ -1,16 +1,16 @@
 import kotlin.random.Random
 
 fun main() { # type: ignore
-    //Listas de dados (usamos 'val' porque as listas em si não mudam)
+    //aqui criamos as listas dos nomes e sobrenomes aleatórios!!!!!1
     val nomes = listOf("Ana", "Bruno", "Carlos", "Diana", "Eduardo")
     val sobrenomes = listOf("Silva", "Santos", "Oliveira", "Souza", "Lima")
 
-    // Sorteia um elemento de cada lista
+    // aqui, criamos uma variavel para sortear um nome e um sobrenome aleatório das listas que criamos
     val nomeSorteado = nomes.random()
     val sobrenomeSorteado = sobrenomes.random()
 
-    // Junta os dois usando String Template
+    // nessa linha, juntamos o nome e sobrenome para ficarem juntinhos!
     val nomeCompleto = "$nomeSorteado $sobrenomeSorteado"
 
-    println("Nome completo gerado: $nomeCompleto")
+    println("Nome completo gerado: $nomeCompleto") //aqui usamos o print line para imprimir o novo nome gameplays
 }
